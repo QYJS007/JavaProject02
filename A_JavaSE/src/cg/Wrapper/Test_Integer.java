6533d7141c380252549950736f96fe43e9a046c9
qyjs <845504836@qq.com>
2018-12-19 20:04:11 +0800

    javaSE intger类型比较

---
+++ A_JavaSE/src/cg/Wrapper/Test_Integer.java
@@ -0,0 +1,19 @@
+package cg.Wrapper;
+
+public class Test_Integer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 1; 
+		Integer integer = new Integer(1);
+		Integer b = 129;
+		
+		System.out.println(a==integer);//true
+		System.out.println(1==integer);//true
+		
+		System.out.println(b==129); //true
+		System.out.println(b.equals(129)); //true
+	}
+
+}
