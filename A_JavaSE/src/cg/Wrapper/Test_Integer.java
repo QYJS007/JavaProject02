404322501cec10514e83bb92c0d3254ffaf0660d
suqin <suqin@bus365.com>
2018-12-20 17:06:56 +0800

    字符串:

--- A_JavaSE/src/cg/Wrapper/Test_Integer.java
+++ A_JavaSE/src/cg/Wrapper/Test_Integer.java
@@ -8,12 +8,15 @@ public class Test_Integer {
 		int a = 1; 
 		Integer integer = new Integer(1);
 		Integer b = 129;
-		
+
 		System.out.println(a==integer);//true
 		System.out.println(1==integer);//true
-		
+
 		System.out.println(b==129); //true
 		System.out.println(b.equals(129)); //true
+		int compareTo = b.compareTo(129);
+
+		System.out.println(compareTo); // 0
 	}
 
 }
---
+++ A_JavaSE/src/cg/string/Test_Split.java
@@ -0,0 +1,13 @@
+package cg.string;
+
+public class Test_Split {
+
+	public static void main(String[] args) {
+		
+		
+		String ss = "1231#";
+		System.out.println(ss.split("#")[0]);//1231
+		System.out.println("123".split("#")[0]);//123s
+		
+	}
+}
